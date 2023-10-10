5e9c96ee033792c7c47add8303addaa5e6d1f21e
Hardikk <kambojpushkal@gmail.com>
2023-10-10 23:37:16 +0530

    functionality added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const circularDivs = document.querySelectorAll('.circular-div');
 const thankyou = document.querySelector('.thankyou-state');
 const first = document.querySelector('.first-state');
+let selectedRating = 3; // Initialize with a default rating
 
 // Function to apply hover styles
 function applyHoverStyles(div) {
@@ -12,9 +13,16 @@ function applyHoverStyles(div) {
 function thankyouvisible(){
     thankyou.style.display = "flex";
     first.style.display = "none";
+    updateDisplayRating(); // Update the displayed rating when the thank you state is shown
 
 
 }
+
+function updateDisplayRating() {
+    const displayRating = document.getElementById('display-rating');
+    displayRating.textContent = selectedRating;
+  }
+
 // Function to reset styles
 function resetStyles(div) {
   div.style.backgroundColor = "#262d37";
@@ -23,30 +31,30 @@ function resetStyles(div) {
 
 // Add event listeners for click, hover, and mouseout
 circularDivs.forEach(div => {
-  let isClicked = false;
-
   // Click event
   div.addEventListener('click', () => {
-    if (isClicked) {
-      resetStyles(div);
-    } else {
-      div.style.backgroundColor = "hsl(25, 97%, 53%)";
-      div.style.color = "#eef3f5";
-    }
-    isClicked = !isClicked;
+    selectedRating = parseInt(div.textContent); // Update the selected rating
+    updateDisplayRating(); // Update the displayed rating
+    resetStyles(div);
+        // Update the background color of the selected button to orange
+        div.style.backgroundColor = "hsl(25, 97%, 53%)";
+        div.style.color = "#eef3f5";
   });
 
   // Hover event
   div.addEventListener('mouseover', () => {
-    if (!isClicked) {
+    if (selectedRating === 0) {
       applyHoverStyles(div);
     }
   });
 
   // Mouseout event
   div.addEventListener('mouseout', () => {
-    if (!isClicked) {
+    if (selectedRating === 0) {
       resetStyles(div);
     }
   });
 });
+
+// Initial update of displayed rating
+updateDisplayRating();
