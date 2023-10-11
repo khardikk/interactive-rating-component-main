0e9e2f4655f08ba514c513c9c3a6f109d9c54f64
Hardikk <kambojpushkal@gmail.com>
2023-10-11 10:03:11 +0530

    bug fix

--- index.js
+++ index.js
@@ -4,43 +4,48 @@ const thankyou = document.querySelector('.thankyou-state');
 const first = document.querySelector('.first-state');
 let selectedRating = 3; // Initialize with a default rating
 
+// Define the setRating function
+function setRating(rating) {
+  selectedRating = rating; // Update the selected rating
+  updateDisplayRating(); // Update the displayed rating
+  resetStyles(); // Reset styles for all circular divs
+  applyActiveStyles(); // Apply active styles to the selected circular div
+}
+
 // Function to apply hover styles
 function applyHoverStyles(div) {
   div.style.backgroundColor = "hsl(217, 12%, 58%)";
   div.style.color = "white";
 }
 
-function thankyouvisible(){
-    thankyou.style.display = "flex";
-    first.style.display = "none";
-    updateDisplayRating(); // Update the displayed rating when the thank you state is shown
-
-
+function thankyouvisible() {
+  thankyou.style.display = "flex";
+  first.style.display = "none";
+  updateDisplayRating(); // Update the displayed rating when the thank you state is shown
 }
 
 function updateDisplayRating() {
-    const displayRating = document.getElementById('display-rating');
-    displayRating.textContent = selectedRating;
-  }
+  const displayRating = document.getElementById('display-rating');
+  displayRating.textContent = selectedRating;
+}
 
-// Function to reset styles
-function resetStyles(div) {
-  div.style.backgroundColor = "#262d37";
-  div.style.color = "hsl(217, 12%, 63%)";
+// Function to reset styles for all circular divs
+function resetStyles() {
+  circularDivs.forEach(div => {
+    div.style.backgroundColor = "#262d37";
+    div.style.color = "hsl(217, 12%, 63%)";
+  });
+}
+
+// Function to apply active styles to the selected circular div
+function applyActiveStyles() {
+  const selectedDiv = circularDivs[selectedRating];
+  selectedDiv.style.backgroundColor = "hsl(25, 97%, 53%)";
+  selectedDiv.style.color = "#eef3f5";
 }
 
 // Add event listeners for click, hover, and mouseout
 circularDivs.forEach(div => {
-  // Click event
-  div.addEventListener('click', () => {
-    selectedRating = parseInt(div.textContent); // Update the selected rating
-    updateDisplayRating(); // Update the displayed rating
-    resetStyles(div);
-        // Update the background color of the selected button to orange
-        div.style.backgroundColor = "hsl(25, 97%, 53%)";
-        div.style.color = "#eef3f5";
-  });
-
   // Hover event
   div.addEventListener('mouseover', () => {
     if (selectedRating === 0) {
@@ -51,7 +56,7 @@ circularDivs.forEach(div => {
   // Mouseout event
   div.addEventListener('mouseout', () => {
     if (selectedRating === 0) {
-      resetStyles(div);
+      resetStyles();
     }
   });
 });
